0c7fdd230cc00ebe2fde8215913d29d7c66dd573
frosch <frosch@a1b688d3-ce07-0410-8a3f-c797401f78de>
2006-01-08 18:33:05 +0000

    - Node hat eine Breite und Höhe bekommen, mit allem was dazu gehört

--- trunk/Sources/source/hapticgraphclasses/Node.cpp
+++ trunk/Sources/source/hapticgraphclasses/Node.cpp
@@ -2,11 +2,14 @@
 /// @file	Node.cpp
 /// @author	Katharina Greiner, Matr.-Nr. 943471
 /// @date	Erstellt am		30.12.2005
-/// @date	Letzte Änderung	03.01.2006
+/// @date	Letzte Änderung	08.01.2006
 //*******************************************************************************
 
 // Änderungen:
 // 03.01.06		- Methode translate() hinzugefügt
+// 08.01.06		- Methoden getHeight(), getWidth(), setHeight(), setWidth() hinzugefügt
+//				  und implementiert
+//				- Konstruktor angepasst
 
 
 #include "Node.h"
@@ -15,8 +18,17 @@
 //*******************************************************************************
 Node::Node( IBusinessAdapter * businessObj )
 {
-	m_pBusinessObject = businessObj;
 	m_DisplayList = 0;
+	m_pBusinessObject = businessObj;
+	if (m_pBusinessObject != NULL)
+	{
+		// Höhe und Breite vom BusinessObjekt geben lassen
+	}
+	else
+	{
+		m_Width = 0.1f;
+		m_Height = 0.1f;
+	}
 }
 //*******************************************************************************
 
@@ -24,7 +36,55 @@ Node::Node( IBusinessAdapter * businessObj )
 Node::~Node()
 {
 	// Displayliste freigeben
-	glDeleteLists(m_DisplayList, 1);	
+	releaseDisplayList();	
+}
+//*******************************************************************************
+
+//*******************************************************************************
+void Node::releaseDisplayList()
+{
+	if (m_DisplayList != 0)
+	{
+		// Displayliste freigeben
+		glDeleteLists(m_DisplayList, 1);
+		m_DisplayList = 0;
+	}
+}
+//*******************************************************************************
+
+//*******************************************************************************
+float Node::getHeight()
+{
+	return m_Height;
+}
+//*******************************************************************************
+
+//*******************************************************************************
+float Node::getWidth()
+{
+	return m_Width;
+}
+//*******************************************************************************
+
+//*******************************************************************************
+void Node::setHeight( float value )
+{
+	m_Height = value;
+
+	// Wenn sich was an der Geometrie ändert, muss die Displayliste neu kompiliert
+	// werden, also erst mal freigeben.
+	releaseDisplayList();
+}
+//*******************************************************************************
+
+//*******************************************************************************
+void Node::setWidth( float value )
+{
+	m_Width = value;
+
+	// Wenn sich was an der Geometrie ändert, muss die Displayliste neu kompiliert
+	// werden, also erst mal freigeben.
+	releaseDisplayList();
 }
 //*******************************************************************************
 
@@ -47,10 +107,16 @@ void Node::renderShape()
         m_DisplayList = glGenLists(1);
         glNewList(m_DisplayList, GL_COMPILE_AND_EXECUTE);
 		glBegin(GL_QUADS);
-		glVertex3f(-0.5f, -0.5f, -0.6f);
-		glVertex3f( 0.5f, -0.5f, -0.6f);
-		glVertex3f( 0.5f,  0.5f, -0.6f);
-		glVertex3f(-0.5f,  0.5f, -0.6f);
+
+		// linke untere Ecke
+		glVertex3f( 0.0f,		-m_Height,	-0.6f);
+		// rechte untere Ecke
+		glVertex3f( m_Width,	-m_Height,	-0.6f);
+		// rechte obere Ecke
+		glVertex3f( m_Width,	 0.0f,		-0.6f);
+		// linke obere Ecke
+		glVertex3f( 0.0f,		 0.0f,		-0.6f);
+
 		glEnd();
         glEndList();
     }
@@ -70,6 +136,9 @@ void Node::translate(const double x, const double y, const double z)
 
 //*******************************************************************************
 void Node::Update( Observable * pObservable )
-{}
+{
+}
 //*******************************************************************************
+
+
 
