eced1a45b3e9ed81cb26af6bf01a34cbf2c13584
erni01 <erni01@a1b688d3-ce07-0410-8a3f-c797401f78de>
2006-01-30 21:45:34 +0000

    Forces update

--- trunk/Sources/source/businesslogic/BusinessTask.cpp
+++ trunk/Sources/source/businesslogic/BusinessTask.cpp
@@ -143,7 +143,7 @@ int BusinessTask::getEnd()
 void BusinessTask::printInfo()
 {
 	cout << this->m_Name << " Dauer: " << this->m_Width << " Beginn: ";
-	cout << m_Begin << " Endet:" << m_End;
+	cout << m_Begin << " Endet:" << m_End << " Final: " << m_Final;
 	cout << " Forces: " << m_ForceRangeIncredible0 << " " << m_ForceRangeMedium0;
 	cout << " " << m_ForceRangeMedium1 << " " << m_ForceRangeIncredible1 << endl;
 }
@@ -358,7 +358,10 @@ void BusinessTask::calcForceMedium0()
 		if (*itObj != NULL)
 		{
 			test = (*itObj)->getBegin() + (*itObj)->getWidth();
-			if (test > range) range = test;
+			if (test > range)
+			{
+				range = test;
+			}
 		}
 	}
 	m_ForceRangeMedium0 = range + 0;
@@ -380,7 +383,7 @@ void BusinessTask::calcForceMedium1()
 		}
 	}
 
-	m_ForceRangeMedium0 = range - m_Width - 1;
+	m_ForceRangeMedium1 = range - m_Width - 0;
 }
 
 
